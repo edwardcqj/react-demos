b70efa27a9fdd782f6182788524de2ba14b95c02
caoqj <caoqj@yonyou.com>
2018-01-12 23:39:17 +0800

    初始化一个Counter组件,认识state

---
+++ demo1/src/Tabs/Tabs.js
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react';
+
+class Tabs extends Component {
+  constructor (props) {
+    super(props);
+  }
+  
+  render () {
+    return (
+      <div className="ui-tabs"></div>
+    )
+  }
+}
+
+export default Tabs;
